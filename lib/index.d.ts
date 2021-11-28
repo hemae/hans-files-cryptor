1f298b300512e4d9a111feee078ba5e7f6a76399
Hans Andersen <61386837+hemae@users.noreply.github.com>
2021-11-29 00:53:19 +0300

    Delete lib directory

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare class FileCrypt {
-    decryptTextFileAndParse<ObjectType>(filePath: string, key: string): ObjectType;
-    encryptObjectAndWriteTextFile(obj: Object, filePath: string, key: string): void;
-}
-declare const _default: FileCrypt;
-export default _default;
