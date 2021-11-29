e095d1708d58b9a64a40afa3c6fabf06ab748cee
Kot <hemae2468@gmail.com>
2021-11-29 03:14:48 +0300

    fix file writing

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
-import {readFileSync, writeFileSync} from 'fs'
+import {readFileSync, writeFileSync, readdirSync, mkdirSync} from 'fs'
 import crypt from 'hans-cryptor'
 
+
+const ENCODING = 'utf8'
+
+function createFile(filePath: string, content: string) {
+    try {
+        writeFileSync(filePath, content, ENCODING)
+    } catch {
+        let path = './'
+        const splitPath = filePath.split('/')
+        for (let item of splitPath) {
+            const dirContent = readdirSync(path)
+            path = path + '/' + item
+            if (dirContent.indexOf(item) === -1) {
+                if (item.split('.').length === 1) {
+                    mkdirSync(path)
+                } else {
+                    writeFileSync(filePath, content, ENCODING)
+                }
+            }
+        }
+    }
+}
 
 class FileCrypt {
     decryptTextFileAndParse<ObjectType>(filePath: string, key: string): ObjectType {
         try {
             return JSON.parse(
                 crypt.decrypt(
-                    readFileSync(filePath, 'utf8'), key
+                    readFileSync(filePath, ENCODING), key
                 )) as ObjectType
         } catch (e) {
             throw e
         }
     }
     encryptObjectAndWriteTextFile(obj: Object, filePath: string, key: string): void {
-        writeFileSync(filePath, crypt.encrypt(JSON.stringify(obj), key), 'utf8')
+        createFile(filePath, crypt.encrypt(JSON.stringify(obj), key))
     }
 }
 
